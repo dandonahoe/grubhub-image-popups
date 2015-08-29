e1545b1d72990087d3fae84ff0931ea8ee8f0d8c
Dan Donahoe <daniel.donahoe@gmail.com>
2015-08-29 13:07:34 -0400

    Got shitty popup working

--- content.js
+++ content.js
@@ -1,16 +1,29 @@
+var mouseX = 0;
+var mouseY = 0;
 
 $(document).ready(function() {
-	
-	console.log("Did this fire?");
-	
+
+	if(window.Event)
+        window.captureEvents(Event.MOUSEMOVE);
+
+    document.onmousemove = function(e) {
+        mouseX = (window.Event) ? e.pageX : event.clientX + document.body.scrollLeft;
+        mouseY = (window.Event) ? e.pageY : event.clientY + document.body.scrollTop;
+
+        console.log('x('+mouseX+') y('+mouseY+')');
+	};
+
+	var hiddenPopup = 
+	    '    <div id="foodie-popup" style="background-color:#FFFDCF;padding:10px;box-shadow: 4px 2px 20px rgba(0, 0, 0, 0.22);width:200px;border-radius:10px;border:1px solid #000000;position:absolute;display:none;text-alignLleft;">'
+	    + 'NOM NOM NOM'
+	    + '    </div>';
+	    
+	$('body').append(hiddenPopup);
 });
-
-
 
 
 $('.sections > .section > .items > li.item').each(function(index) {
 	
-
 	$(this).hoverIntent(function(e) {
 		var itemName = $(this).find('.name').text();
 		console.log("Food item name(" + itemName + ")");
@@ -22,7 +35,7 @@ $('.sections > .section > .items > li.item').each(function(index) {
 		    url:searchUrl,
 		    dataType: "json",
 		    success: function(response) {
-		        processImageSearchResults(response);
+		        processImageSearchResults(response, itemName);
 		    }
 	    });
 	}, function() {
@@ -31,10 +44,19 @@ $('.sections > .section > .items > li.item').each(function(index) {
 });
 
 
-function processImageSearchResults(response) {
+function processImageSearchResults(response, itemName) {
 	var responseData = response.responseData;
 
 	var results = responseData.results;
 
 	alert(results[0].tbUrl);//teakdoor.com/Gallery/albums/userpics/10004/Thai_stir_fried_beef_holy_basil_pad_ka_pow.jpg);
+
+
+	$('#foodie-popup').css('left', (mouseX - 200 / 2) + 'px');
+    $('#foodie-popup').css('top',  (mouseY + 24) + 'px');
+    //$('#foodie-popup-title').html(popupHeader);
+    //$('#foodie-popup-body').html(definitionData.Definition + debugFooter);
+    $('#foodie-popup').fadeIn();
+	
+
 }
