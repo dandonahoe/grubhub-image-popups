637d3bb21db1f22716c8e21850c772ab84f8acf0
Dan Donahoe <daniel.donahoe@gmail.com>
2015-08-29 12:56:25 -0400

    Successfully makes image lookup vis google image search

--- content.js
+++ content.js
@@ -14,5 +14,19 @@ $('.sections > .section > .items > li.item').each(function(index) {
 	$(this).hoverIntent(function(e) {
 		var itemName = $(this).find('.name').text();
 		console.log("Food item name(" + itemName + ")");
+
+		var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + encodeURIComponent(itemName);
+
+		$.ajax({
+		    type: "GET",
+		    url:searchUrl,
+		    dataType: "json",
+		    success: function(json) {
+		        //definitionData.Definition = $(xml).find("Description").text();
+		        // /successfulLookup(definitionData);
+alert(json);
+		        console.log("Success?");
+		    }
+	    });
 	});
 });
