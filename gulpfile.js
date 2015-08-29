352337defffd434b4c8460a976dceeb66992fe5d
Dan Donahoe <daniel.donahoe@gmail.com>
2015-08-29 12:14:17 -0400

    Added hoverintent to gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,18 @@ gulp.task('copyFiles', function () {
 
 	gulp.src('./node_modules/jquery/dist/jquery.min.js')
 		.pipe(gulp.dest('dist'));
+
+	gulp.src('./bower_components/jquery-hoverintent/jquery.hoverIntent.js')
+		.pipe(gulp.dest('dist'));
+
 });
 
 gulp.task('watch', function() {
-	gulp.watch('./node_modules/**/*', ['copyFiles']);
+	gulp.watch([
+		'./node_modules/**/*',
+		'./bower_components/**/*',
+	], [
+		'copyFiles'
+	]);
 });
+
