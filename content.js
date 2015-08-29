c71561f8ac74b89a6fe7e386406df1a08669f9ec
Dan Donahoe <daniel.donahoe@gmail.com>
2015-08-29 15:04:27 -0400

    Wikipedia lookup kinda works again

--- content.js
+++ content.js
@@ -17,30 +17,72 @@ $('.sections > .section > .items > li.item').each(function() {
 
 	var itemName = $(this).find('.name').text();
 
+
+	// $.ajax({
+	//     type: "GET",
+	//     url: "https://en.wikipedia.org/w/api.php?action=opensearch&limit=1&format=json&search=" + encodeURIComponent(itemName),
+	//     dataType: "json",
+	//     success: function(json) {
+	//     	alert(json.length);
+	//         //definitionData.Definition = $(xml).find("Description").text();
+	//         //successfulLookup(definitionData);
+	//     }
+ //    });
+
 	$(this).qtip({
+		
 	    content: {
-	        text: 'Loading...',
-	        title: itemName,
-	        ajax: {
-	            url: 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + encodeURIComponent(itemName),
-	            type: 'GET', // POST or GET
-	            dataType: "json",
-	            success: function(response, status) {
-	            	var responseData = response.responseData;
+	        text: function(event, api) {
+	        	$.ajax({
+	                type: "GET",
 
-					var results = responseData.results;
+	                url: "https://en.wikipedia.org/w/api.php?action=opensearch&limit=1&format=json&search=" + itemName,
+	                dataType: "json"
 
-					
-					var popupHtml = ''
-					    + '<div id="foodie-popup-body" style="">'
-					    + '<img src="' + results[0].tbUrl + '" alt="' + itemName + '" />'
-					    + '</div>';
-					
-	            	this.set('content.text', popupHtml);//$("#foodie-pupup").html());
+	            }).then(function(content) {
+	                // Set the tooltip content upon successful retrieval
+	                
+	                var itemLookupName = $.trim(content[1]);
+	                var wikiDefinition = "";
+	                var wikiPageUrl = "";
 
+	                if(itemLookupName !== "") {
+	                	wikiDefinition = content[2][0];
+	                	wikiPageUrl = content[3];
+	                }
 
-	            }
+	                console.log("wikipage(" + wikiPageUrl + "), def(" + wikiDefinition + ")");
+
+	            }, function(xhr, status, error) {
+	                // Upon failure... set the tooltip content to the status and error value
+	                //api.set('content.text', status + ': ' + error);
+	                console.log("Wikipedia lookup failed");
+	            });
+
+	            return "Loading....";
 	        }
+
+	        
+	    //     ajax: {
+	    //         url: 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + encodeURIComponent(itemName),
+	    //         type: 'GET', // POST or GET
+	    //         dataType: "json",
+	    //         success: function(response, status) {
+	    //         	var responseData = response.responseData;
+
+					// var results = responseData.results;
+
+					
+					// var popupHtml = ''
+					//     + '<div id="foodie-popup-body" style="">'
+					//     + '<img src="' + results[0].tbUrl + '" alt="' + itemName + '" />'
+					//     + '</div>';
+					
+	    //         	this.set('content.text', popupHtml);//$("#foodie-pupup").html());
+
+
+	    //         }
+	    //     }
 	    },
 	    position: {
 	        viewport: $(window)
